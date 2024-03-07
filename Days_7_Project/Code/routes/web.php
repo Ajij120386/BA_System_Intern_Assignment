2563a54b5c2818443ca2919605235ebfe09d96a3
Ajij <mohammadajij120386@gmail.com>
2024-03-07 10:55:04 +0600

    ajij

--- Days_7_Project/Code/routes/web.php
+++ Days_7_Project/Code/routes/web.php
@@ -1,12 +1,26 @@
-
 <?php
- 
+
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\CustomersController; //add CustomersController
- 
-//Route::get('/', function () {
-//    return view('welcome');
-//});
-   
-Route::get('/', [CustomersController::class, 'index']);
-Route::get('ajaxdata', [CustomersController::class, 'getdata'])->name('getdata');
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\PdfController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [UserController::class, 'index']);
+Route::get('ajaxdata', [UserController::class, 'getdata'])->name('getdata');
+Route::get('generate-pdf', [PdfController::class, 'generatePdf']);
+
+
